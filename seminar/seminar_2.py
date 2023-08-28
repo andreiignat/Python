2064f3eed8cf94461152dc4a32a3b759e26d7a21
andrei ignat <>
2023-08-28 22:28:54 +0300

    homeWork_seminar_2

---
+++ seminar/seminar_2.py
@@ -0,0 +1,26 @@
+# N факториал
+
+'''''
+n = int(input())
+ 
+factorial = 1
+while n > 1:
+    factorial *= n
+    n -= 1
+ 
+print(factorial)
+
+'''
+# Находим является ли введенное число, числом фибоначи
+
+# n = int(input("введите число "))
+# a=b=1
+# c=a+b
+# while c<=n:
+#     a=b+c
+#     a,b,c=b,c,a
+# print(b==n)
+
+
+
+
